ea067cdd73cb15a7a6388e4a1f36214cb778bd98
German Barron <germanbarrona12@gmail.com>
2026-05-07 20:35:01 -0700

    feat: CRUD completo de roles y usuarios con relacion BD

---
+++ backend/bd.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user:     'postgres',
+  host:     'localhost',
+  database: 'escuela',
+  password: 'jona',
+  port:     5432
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error al conectar con la base de datos:', err.message);
+  } else {
+    console.log('Conectado a la base de datos correctamente');
+    release();
+  }
+});
+
+module.exports = pool;

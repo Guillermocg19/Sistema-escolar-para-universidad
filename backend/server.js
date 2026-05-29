2e4ade13674c9c15ecdd04b6eb066787e149ad75
German Barron <germanbarrona12@gmail.com>
2026-05-28 23:00:45 -0700

    fix: se extendio el limite para subir documentos de aspirantes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const aspirantesRouter     = require('./routes/aspirantes');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'})); 
+app.use(express.urlencoded({ extended: true, limit: '10mb' })); 
 
 app.use('/usuarios',       usuariosRouter);
 app.use('/roles',          rolesRouter);
